3b8fa0a3d1b425a54878f809b20d63fdf80044c1
josuaobando <Josh2909>
2018-05-06 17:24:57 -0600

    update

--- cronjobs/start.php
+++ cronjobs/start.php
@@ -1,39 +1,46 @@
 #!/usr/local/bin/php
 <?php
 
-ini_set("include_path", ".:/var/www/api.test.dinerosegurohf.com/http");
-ini_set("display_errors", "on");
-ini_set("error_reporting", "6135");
+try{
 
-require_once 'system/Startup.class.php';
+  ini_set("include_path", ".:/var/www/api.test.dinerosegurohf.com/http");
 
-/**
- * customer function for logging
- *
- * @param string $file
- * @param string $message
- * @param array $args
- */
-function custom($file, $message, $args = null)
-{
-  $datetime = date('Y-m-d H:i:s');
+  ini_set("short_open_tag", "on");
+  ini_set("display_errors", "on");
+  ini_set("error_reporting", "6135");
 
-  //replace variables if there is any
-  if($args && is_array($args)){
-    foreach($args as $key => $value){
-      $message = str_replace("{" . $key . "}", $value, $message);
+  require_once '../system/Startup.class.php';
+
+  /**
+   * customer function for logging
+   *
+   * @param string $file
+   * @param string $message
+   * @param array $args
+   */
+  function custom($file, $message, $args = null)
+  {
+    $datetime = date('Y-m-d H:i:s');
+
+    //replace variables if there is any
+    if($args && is_array($args)){
+      foreach($args as $key => $value){
+        $message = str_replace("{".$key."}", $value, $message);
+      }
     }
+
+    $content = $format = "[%{datetime}] %{message} \n";
+    $content = str_replace("%{datetime}", $datetime, $content);
+    $content = str_replace("%{message}", $message, $content);
+
+    $logFile = "/log-".$file.'.log';
+
+    @file_put_contents('/var/www/api.test.dinerosegurohf.com/http/logs'.$logFile, $content, FILE_APPEND);
   }
 
-  $content = $format = "[%{datetime}] %{message} \n";
-  $content = str_replace("%{datetime}", $datetime, $content);
-  $content = str_replace("%{message}", $message, $content);
-
-  $logFile = "/log-" . $file . '.log';
-
-  @file_put_contents('/var/www/api.test.dinerosegurohf.com/http/logs' . $logFile, $content, FILE_APPEND);
+  custom('cronjob', 'Jobs has running');
+}catch(Exception $e){
+  echo $e->getMessage();
 }
-
-custom('cronjob', 'Jobs has running');
 
 ?>
