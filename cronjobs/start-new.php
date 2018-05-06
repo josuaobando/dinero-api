e2038f7711b3ee10035bcf2a208e912d3353ee70
josuaobando <Josh2909>
2018-05-06 17:51:50 -0600

    update

--- cronjobs/start-new.php
+++ cronjobs/start-new.php
@@ -1,4 +1,4 @@
-#!#!/usr/bin/php
+#!/usr/bin/php
 <?php
 
 try{
@@ -11,36 +11,9 @@ try{
 
   require_once '../system/Startup.class.php';
 
-  /**
-   * customer function for logging
-   *
-   * @param string $file
-   * @param string $message
-   * @param array $args
-   */
-  function custom($file, $message, $args = null)
-  {
-    $datetime = date('Y-m-d H:i:s');
-
-    //replace variables if there is any
-    if($args && is_array($args)){
-      foreach($args as $key => $value){
-        $message = str_replace("{".$key."}", $value, $message);
-      }
-    }
-
-    $content = $format = "[%{datetime}] %{message} \n";
-    $content = str_replace("%{datetime}", $datetime, $content);
-    $content = str_replace("%{message}", $message, $content);
-
-    $logFile = "/log-".$file.'.log';
-
-    @file_put_contents('/var/www/api.test.dinerosegurohf.com/http/logs'.$logFile, $content, FILE_APPEND);
-  }
-
-  custom('cronjob', 'Jobs has running');
+  Log::custom('cronjob', 'Jobs has running');
 }catch(Exception $e){
-  echo $e->getMessage();
+  Log::custom('cronjob', $e->getMessage());
 }
 
 ?>
