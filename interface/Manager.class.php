916f5c330fac58f475cfc56abae69b5abdc9c5b9
josuaobando <Josh2909>
2018-05-13 12:31:39 -0600

    - add message to cancelled trx

--- interface/Manager.class.php
+++ interface/Manager.class.php
@@ -308,7 +308,11 @@ class Manager
     $wsRequest->putParam('type', $transaction->getAgencyTypeId());
 
     if($transaction->getTransactionStatusId() != Transaction::STATUS_REQUESTED && $transaction->getTransactionStatusId() != Transaction::STATUS_REJECTED){
-      throw new InvalidStateException("Transaction cannot be confirmed since the current status is: " . $transaction->getTransactionStatus());
+      if($transaction->getTransactionStatusId() != Transaction::STATUS_CANCELED){
+        throw new InvalidStateException("The transaction has expired. Valid time is 48 hours to confirm.");
+      }else{
+        throw new InvalidStateException("Transaction cannot be confirmed since the current status is: " . $transaction->getTransactionStatus());
+      }
     }
 
     //validate customer
