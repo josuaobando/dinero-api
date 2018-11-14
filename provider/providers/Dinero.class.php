77a7f26ab92149bfb378ce4b71237416a4015354
josuaobando <Josh2909>
2018-11-14 15:12:13 -0600

    + removed validation

--- provider/providers/Dinero.class.php
+++ provider/providers/Dinero.class.php
@@ -62,9 +62,11 @@ class Dinero extends Provider
     $customer = Session::getCustomer();
     $transaction = Session::getTransaction();
 
+    /*
     if($customer->getIsAPI()){
       throw new P2PException("Redirect to API...");
     }
+    */
 
     //validate if customer is blacklisted
     $customer->isBlacklisted();
