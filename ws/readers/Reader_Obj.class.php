743ca6341196e9b95de672bb5aa79f76ca3ca4e9
Josua <josua.obando@gmail.com>
2018-11-17 13:24:02 -0600

    + add reader

---
+++ ws/readers/Reader_Obj.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author Josua
+ */
+class Reader_Obj extends Reader
+{
+
+  /**
+   * @see Reader::parse()
+   *
+   * @param string $data
+   *
+   * @return mixed
+   *
+   * @throws WSException
+   */
+  public function parse($data)
+  {
+    $simpleXml = simplexml_load_string($data);
+    if(!($simpleXml instanceof SimpleXMLElement)){
+      return json_decode($data);
+    }
+    return $simpleXml;
+  }
+
+}
+
+?>
